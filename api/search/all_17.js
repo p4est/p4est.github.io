7dc9267136ed3b318f07c75595b6961ddd7b9f8f
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2014-07-14 18:28:11 +0200

    Bump api documentation

--- api/search/all_17.js
+++ api/search/all_17.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['z',['z',['../structp8est__quadrant__t.html#afc46f76cc3532d98f59b9a6900aa7318',1,'p8est_quadrant_t']]]
+  ['z',['z',['../structp8est__quadrant__t.html#afc46f76cc3532d98f59b9a6900aa7318',1,'p8est_quadrant_t']]],
+  ['zero',['zero',['../p4est__step4_8c.html#a81f7a9620140cc7a53fc197a36dc2e81',1,'p4est_step4.c']]]
 ];
--- api/search/variables_2.js
+++ api/search/variables_2.js
@@ -3,7 +3,9 @@ var searchData=
   ['center',['center',['../structstep3__ctx__t.html#a05eccaecdcde45a98e95f45d30b1ca2b',1,'step3_ctx_t']]],
   ['connectivity',['connectivity',['../structp4est__t.html#a1b1f1c015ef6de48b4538c8b6a7fc970',1,'p4est_t::connectivity()'],['../structp8est__t.html#a1d6095d2068f593e8e6a15fe8bbfb4fb',1,'p8est_t::connectivity()']]],
   ['corner',['corner',['../structp4est__iter__corner__side__t.html#a3c7fc99263529fa2510a767b568c60b0',1,'p4est_iter_corner_side_t::corner()'],['../structp8est__iter__corner__side__t.html#a7818ae16b609c329dc97ffd2334e1997',1,'p8est_iter_corner_side_t::corner()']]],
+  ['corner_5fnum_5fhanging',['corner_num_hanging',['../p4est__step4_8c.html#acded771b870059751275f8f67b9b6b8d',1,'p4est_step4.c']]],
   ['corner_5fto_5fcorner',['corner_to_corner',['../structp4est__connectivity__t.html#a70476dcd3f59aaa2bf14f0ef88b93bde',1,'p4est_connectivity_t::corner_to_corner()'],['../structp8est__connectivity__t.html#a3bd9a461f49e3f05a15974960f6b6376',1,'p8est_connectivity_t::corner_to_corner()']]],
+  ['corner_5fto_5fhanging',['corner_to_hanging',['../p4est__step4_8c.html#ad586eb01be2684ef34d8919c0783fb5e',1,'p4est_step4.c']]],
   ['corner_5fto_5ftree',['corner_to_tree',['../structp4est__connectivity__t.html#a1542ba61fe19b0c4c8ded719b43df7f5',1,'p4est_connectivity_t::corner_to_tree()'],['../structp8est__connectivity__t.html#ae27f00281e21c2526df09526cbdb1a5b',1,'p8est_connectivity_t::corner_to_tree()']]],
   ['ctt_5foffset',['ctt_offset',['../structp4est__connectivity__t.html#a67cf273f0c3358898735c192a607ecf3',1,'p4est_connectivity_t::ctt_offset()'],['../structp8est__connectivity__t.html#a72b4e5a11d696a2b6bac3f1e9f156408',1,'p8est_connectivity_t::ctt_offset()']]]
 ];
