1dcdfdd8f3d70e9a4bf78038dab1968359766576
Carsten Burstedde <burstedde@ins.uni-bonn.de>
2025-03-20 21:07:31 +0100

    Update libsc documentation to v2.8.7

---
+++ api/libsc-2.8.7/search/all_2.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['buffer_9',['buffer',['../structsc__io__sink.html#ae2b8863bac9321060cd966d395786edf',1,'sc_io_sink::buffer()'],['../structsc__io__source.html#aeb91669383e263aa4e14a67514b19f74',1,'sc_io_source::buffer()'],['../structsc__string.html#aff49d82ca43ec7a7427c354400cfa585',1,'sc_string::buffer()']]],
+  ['buffer_5fbytes_10',['buffer_bytes',['../structsc__io__sink.html#a31a369c94d9e16cfe3497e7199b90a02',1,'sc_io_sink::buffer_bytes()'],['../structsc__io__source.html#aea29cc777b5e40dbcc9c7110353c1ac4',1,'sc_io_source::buffer_bytes()']]],
+  ['byte_5falloc_11',['byte_alloc',['../structsc__array.html#ae0a0a01284b54c206ff71dedd07bfb85',1,'sc_array']]],
+  ['bytes_5fin_12',['bytes_in',['../structsc__io__sink.html#a27c5794f4c1d395580589d48717fb393',1,'sc_io_sink::bytes_in()'],['../structsc__io__source.html#ac17d595b53cadc79d2ad349e5aab6e43',1,'sc_io_source::bytes_in()']]],
+  ['bytes_5fout_13',['bytes_out',['../structsc__io__sink.html#ad12b9178dfe6fab215294b13e1c7275b',1,'sc_io_sink::bytes_out()'],['../structsc__io__source.html#a3f7e0eb7e23e623e68b5b8a601bce7a9',1,'sc_io_source::bytes_out()']]]
+];
---
+++ api/libsc-2.8.7/search/all_4.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['data_19',['data',['../structsc__link.html#a7803c18e4a81f274791cbe261e009ead',1,'sc_link']]],
+  ['deprecated_20list_20',['Deprecated List',['../deprecated.html',1,'']]],
+  ['dirty_21',['dirty',['../structsc__statinfo.html#a841f217bffd3c8421a9e13e33c50777f',1,'sc_statinfo']]]
+];
